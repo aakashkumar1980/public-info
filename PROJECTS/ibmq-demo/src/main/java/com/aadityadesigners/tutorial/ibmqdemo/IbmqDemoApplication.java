c3ae052840a9e6cc55c23ed6ef7eb4981393fb45
Aakash Kumar <aakash.kumar1980@yahoo.com>
2023-06-23 07:39:26 +0000

    base setup

---
+++ PROJECTS/ibmq-demo/src/main/java/com/aadityadesigners/tutorial/ibmqdemo/IbmqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aadityadesigners.tutorial.ibmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmqDemoApplication.class, args);
+	}
+
+}
---
+++ PROJECTS/ibmq-demo/src/test/java/com/aadityadesigners/tutorial/ibmqdemo/IbmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadityadesigners.tutorial.ibmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
